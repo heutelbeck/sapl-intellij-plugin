db586c48182a72a643297f1a3db3934a10c2073b
Ole Ludwig <o.ludwig@wtnet.de>
2024-02-18 22:29:42 +0100

    replace with svg icon

--- src/main/kotlin/com/github/oleludwig/saplintellijplugin/SaplIcons.kt
+++ src/main/kotlin/com/github/oleludwig/saplintellijplugin/SaplIcons.kt
@@ -3,5 +3,5 @@ package com.github.oleludwig.saplintellijplugin
 import com.intellij.openapi.util.IconLoader
 
 object SaplIcons {
-    val SAPL = IconLoader.getIcon("/icons/sapl.png", javaClass)
+    val SAPL = IconLoader.getIcon("/icons/sapl-icon.svg", javaClass)
 }
